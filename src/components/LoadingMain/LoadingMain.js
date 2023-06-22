cc94ce662eef0c876c5fd0380b1d568e16544be7
Igor Vovk <ovodigr@gmail.com>
2023-06-22 23:12:02 +0300

    add loader

--- src/components/LoadingMain/LoadingMain.js
+++ src/components/LoadingMain/LoadingMain.js
@@ -1,5 +1,24 @@
+import { ProgressBar } from 'react-loader-spinner';
+
 const LoadingMain = () => {
-    return <div className="load">Loading..... Please wait</div>
-}
+  const style = {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  };
+
+  return (
+    <ProgressBar
+      height="280"
+      width="280"
+      ariaLabel="progress-bar-loading"
+      wrapperStyle={style}
+      wrapperClass="progress-bar-wrapper"
+      borderColor="#F4442E"
+      barColor="#51E5FF"
+    />
+  );
+};
 
 export default LoadingMain;
